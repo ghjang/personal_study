56afc559b01012fe88591e4a341e4dfe870f0520
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-09 19:38:04 +0900

    added decay test codes.

---
+++ cpp/decay/main.cpp
@@ -0,0 +1,130 @@
+#include <type_traits>
+#include <cassert>
+#include <iostream>
+
+
+template <typename T>
+T func(T t);
+
+
+template <typename T>
+auto ReturnDecayedValue(T t)
+{
+    return t;
+}
+
+template <typename T>
+auto ReturnDecayedValue1(T t) -> decltype(t)
+{
+    return t;
+}
+
+template <typename T>
+auto ReturnDecayedValue2(T & t)
+{
+    return t;
+}
+
+template <typename T>
+auto ReturnDecayedValue3(T & t) -> decltype(t)
+{
+    return t;
+}
+
+
+int f(int i) { return i + 1; }
+
+
+int main() {
+    using std::is_same;
+    using std::is_array;
+    using std::remove_reference;
+    using std::decay;   // C++11
+    using std::decay_t; // C++14
+
+    int i = 100;
+    int & ri = i;
+    int const j = 200;
+    int const volatile k = 300;
+
+    static_assert(is_same<int, decltype(func(i))>::value, "");
+    static_assert(is_same<int, decltype(func(ri))>::value, "");
+    static_assert(is_same<int, decltype(func(j))>::value, "");
+    static_assert(is_same<int, decltype(func(k))>::value, "");
+
+    // int array to pointer decay
+    int a[3] = { 1, 2, 3 };
+    int * pa = a;
+    static_assert(is_same<decltype(a), int[3]>::value, "");
+    static_assert(is_same<decltype(pa), int *>::value, "");
+    static_assert(is_same<decltype(pa), decay<decltype(a)>::type>::value, "");
+    static_assert(is_same<decltype(pa), decay_t<decltype(a)>>::value, "");
+
+    // multi-dimensional array to pointer decay
+    int aa[2][3] = {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+    };
+    int (* paa) [3] = aa; // loses top-level dimension size information, 2.
+    static_assert(is_same<decltype(aa), int [2][3]>::value, "");
+    static_assert(is_same<decltype(paa), int (*) [3]>::value, ""); // pointer to 'int [3]'
+    paa[1][0] = 44;
+    assert(aa[1][0] == 44);
+
+    // const char array to pointer decay
+
+    {
+        using abc_t = decltype("abc");
+        abc_t c = "abc";
+        char const * pc = c;
+
+        static_assert(is_same<decltype("abc"), char const (&) [4]>::value, ""); // reference to 'char const [4]'
+        static_assert(is_same<abc_t, char const (&) [4]>::value, "");
+        static_assert(is_array<remove_reference<abc_t>::type>::value, "");
+
+        static_assert(is_same<decltype(pc), decay<abc_t>::type>::value, "");
+        static_assert(is_same<decltype(pc), decay_t<abc_t>>::value, "");
+    }
+
+    {
+        char const c[] = "abc";
+        char const * pc = c;
+        static_assert(is_same<decltype(c), char const [4]>::value, "");
+        static_assert(is_same<decltype(pc), decay<decltype(c)>::type>::value, "");
+        static_assert(is_same<decltype(pc), decay_t<decltype(c)>>::value, "");
+
+        // const can be removed at this context.
+        char c1[] = "defg";
+        static_assert(is_same<decltype(c1), char [5]>::value, "");
+    }
+
+    // function-to-pointer decay
+    auto pf = f;
+    int (&rf) (int) = f;
+    int (*pf1) (int) = f;
+    static_assert(is_same<int (int), decltype(f)>::value, "");                  // function type
+    static_assert(is_same<int (&) (int), decltype(rf)>::value, "");             // function reference type
+    static_assert(is_same<int (*) (int), decltype(pf)>::value, "");             // function pointer type
+    static_assert(is_same<int (*) (int), decay<decltype(f)>::type>::value, "");
+    static_assert(is_same<int (*) (int), decay_t<decltype(f)>>::value, "");
+
+    static_assert(is_same<int *, decltype(ReturnDecayedValue(a))>::value, "");
+    static_assert(is_same<int (*) [3], decltype(ReturnDecayedValue(aa))>::value, "");
+    static_assert(is_same<int (*) (int), decltype(ReturnDecayedValue(f))>::value, "");
+
+    static_assert(is_same<int *, decltype(ReturnDecayedValue1(a))>::value, "");
+    static_assert(is_same<int (*) [3], decltype(ReturnDecayedValue1(aa))>::value, "");
+    static_assert(is_same<int (*) (int), decltype(ReturnDecayedValue1(f))>::value, "");
+
+    // ???
+    static_assert(is_same<int *, decltype(ReturnDecayedValue2(a))>::value, "");
+    static_assert(is_same<int (*) [3], decltype(ReturnDecayedValue2(aa))>::value, "");
+    static_assert(is_same<int (*) (int), decltype(ReturnDecayedValue2(f))>::value, "");
+
+    static_assert(is_same<int (&) [3], decltype(ReturnDecayedValue3(a))>::value, "");       // reference to 'int [3]'
+    static_assert(is_same<int (&) [2][3], decltype(ReturnDecayedValue3(aa))>::value, "");   // reference to 'int [2][3]'
+    static_assert(is_same<int (&) (int), decltype(ReturnDecayedValue3(f))>::value, "");     // reference to 'int (int)'
+
+    return 0;
+}
+
