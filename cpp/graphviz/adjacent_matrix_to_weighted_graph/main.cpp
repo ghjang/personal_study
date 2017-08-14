386202d3e2784285e65e7dc169869825f2efc9dc
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-08-14 23:07:17 +0900

    implemented adjacent_matrix_to_weighted_graph function.

--- cpp/graphviz/adjacent_matrix_to_weighted_graph/main.cpp
+++ cpp/graphviz/adjacent_matrix_to_weighted_graph/main.cpp
@@ -1,13 +1,29 @@
 #include <array>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 
 template <typename T>
 auto adjacent_matrix_to_weighted_graph(T && mat, std::size_t row, std::size_t col)
-        -> decltype(mat[row][col], std::string{})
+        -> decltype(mat[0][0], std::string{})
 {
-    return "";
+    std::ostringstream oss;
+
+    oss << "digraph\n"
+        << "{\n";
+
+    for (std::size_t i = 0; i < row; ++i) {
+        for (std::size_t j = 0; j < col; ++j) {
+            if (mat[i][j] != 0) {
+                oss << '\t' << i << " -> " << j << "[label=" << mat[i][j] << ", weight=" << mat[i][j] << "];" << '\n';
+            }
+        }
+    }
+
+    oss << "}\n";
+
+    return oss.str();
 }
 
 template <typename T, std::size_t row, std::size_t col>
@@ -26,11 +42,11 @@ auto adjacent_matrix_to_weighted_graph(std::array<std::array<T, col>, row> && ma
 int main(int, char**)
 {
     std::array<std::array<int, 5>, 5> mat = {{
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 }
+        { 0, 2, 0, 0, 0 },
+        { 0, 0, 0, 5, 0 },
+        { 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 3, 0 },
+        { 1, 0, 4, 0, 0 }
     }};
 
     std::cout << adjacent_matrix_to_weighted_graph(mat) << '\n';
