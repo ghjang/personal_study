7cf159b73320595604bc06fdfab3d99f4cfa2e02
DESKTOP-KQUB8HD\ghjang <jmxexpert@hotmail.com>
2022-07-24 17:36:02 +0900

    'DISTINCT'와 'PARTITION BY' 절을 제거하고 'GROUP BY' 절을 이용해 '구구단' 출력을 하는 예시 SQL문 추가

--- oracle_db/불친절한_SQL_프로그래밍/구구단_수평출력.sql
+++ oracle_db/불친절한_SQL_프로그래밍/구구단_수평출력.sql
@@ -1,3 +1,4 @@
+-- 굳이 'PARTITION BY' 절을 사용해서 작성함.
 WITH xs AS (
 	SELECT (LEVEL + 1) AS n
 	FROM DUAL
@@ -16,3 +17,26 @@ SELECT DISTINCT LISTAGG(f.a || 'x' || f.b || '=' || f.a * f.b, '      ')
 					WITHIN GROUP(ORDER BY f.a)
 					OVER(PARTITION BY f.b)
 FROM fs f;
+
+
+-- 위의 쿼리에서 'DISTINCT'와 'PARTITION BY' 절을 제거함.
+WITH xs AS (
+	SELECT (LEVEL + 1) AS n
+	FROM DUAL
+	CONNECT BY LEVEL < 9
+)
+, ys AS (
+	SELECT n FROM xs
+		UNION
+	SELECT 1 FROM DUAL
+)
+, fs AS (
+	SELECT x.n AS a, y.n AS b
+	FROM xs x, ys y
+)
+SELECT LISTAGG(
+			f.a || 'x' || f.b || '=' || f.a * f.b,
+			'      '
+		) WITHIN GROUP(ORDER BY f.a)
+FROM fs f
+GROUP BY f.b;
