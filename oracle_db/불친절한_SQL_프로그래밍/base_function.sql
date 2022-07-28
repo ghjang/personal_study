bd43a3cf07993869df8b6a878026e5c44943e976
DESKTOP-KQUB8HD\ghjang <jmxexpert@hotmail.com>
2022-07-28 13:35:57 +0900

    'DEBUG_PRINT' 함수 추가

--- oracle_db/불친절한_SQL_프로그래밍/base_function.sql
+++ oracle_db/불친절한_SQL_프로그래밍/base_function.sql
@@ -1,3 +1,17 @@
+-- 'CS'를 'Output' 콘솔 윈도에 출력한다.
+CREATE OR REPLACE PROCEDURE DEBUG_PRINT(rows CS)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('rows.COUNT = ' || rows.COUNT);
+
+    FOR i IN 1..rows.COUNT LOOP
+        DBMS_OUTPUT.put_line(rows(i));
+    END LOOP;
+
+    DBMS_OUTPUT.PUT(CHR(10));   -- newline
+END;
+
+
 -- '등차수열'의 n번째 항을 리턴
 CREATE OR REPLACE FUNCTION NEXT_ARITHMETIC_N(init IN INTEGER, diff IN INTEGER, n IN INTEGER) RETURN INTEGER
 IS
