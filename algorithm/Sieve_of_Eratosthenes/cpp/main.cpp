cb4df1bc6e405214c3242f81771badc18e2cf478
Gil Ho, Jang <jmxexpert@hotmail.com>
2023-02-24 13:56:32 +0900

    '100' 미만의 '소수'를 구는 단순 C++ 코드 추가

--- algorithm/Sieve_of_Eratosthenes/cpp/main.cpp
+++ algorithm/Sieve_of_Eratosthenes/cpp/main.cpp
@@ -1,6 +1,37 @@
+#include <vector>
 #include <iostream>
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int const n = 100;
+
+    std::vector<bool> v(n, true);
+
+    v[0] = false;   // '0' is not a prime number.
+    v[1] = false;   // '1' is not a prime number.
+
+    for (int i = 2; i < n; ++i) {
+        // move to the next prime number.
+        for (int j = i; j < n; ++j) {
+            if (v[j]) {
+                i = j;
+                break;
+            }
+        }
+
+        // mark all multiples of the current prime number as non-prime.
+        for (int j = i + 1; j < n; ++j) {
+            if (j % i == 0) {
+                v[j] = false;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (v[i]) {
+            std::cout << i << " ";
+        }
+    }
+
     return 0;
 }
